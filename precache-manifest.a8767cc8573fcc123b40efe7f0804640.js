496c77f901ef5dbf9c8069410d9ff8f504d8a87c
Aline Ferreira <aline.ferreira@bpf.com.br>
2020-05-27 15:51:05 -0300

    Updates

---
+++ precache-manifest.a8767cc8573fcc123b40efe7f0804640.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d583ff0ec1ded7dcaab85d12d552798f",
+    "url": "/findLyrics-react/index.html"
+  },
+  {
+    "revision": "6b5c0153fa16a0dc5341",
+    "url": "/findLyrics-react/static/css/main.c71eaed6.chunk.css"
+  },
+  {
+    "revision": "8a02e4016650fcac5e40",
+    "url": "/findLyrics-react/static/js/2.72551bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findLyrics-react/static/js/2.72551bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5c0153fa16a0dc5341",
+    "url": "/findLyrics-react/static/js/main.ce89a85b.chunk.js"
+  },
+  {
+    "revision": "20c99a381a836d10d7cf",
+    "url": "/findLyrics-react/static/js/runtime-main.e387c4bf.js"
+  }
+]);
